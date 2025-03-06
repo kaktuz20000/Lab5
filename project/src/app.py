533c9279a5a8816699d49da5244b10372b6f57d9
kaktuz20000 <149298757+kaktuz20000@users.noreply.github.com>
2025-03-06 12:24:16 +0300

    дорогой дневник я пережил ужасные события в своей жизни их даже не описать

--- project/src/app.py
+++ project/src/app.py
@@ -6,5 +6,5 @@ app = FastAPI()
 
 
 @app.get("/calculate/{a}/{b}")
-def calculate_endpoint(a: int, b: int):
+def calculate_endpoint(a: int, b: int) -> dict[str, int]:
     return {"result": calculate(a, b)}
--- project/tests/app_test.py
+++ project/tests/app_test.py
@@ -1,6 +1,6 @@
 from src.functional import calculate
 
 
-def test_calculate():
+def test_calculate() -> None:
     assert calculate(2, 3) == 5
     assert calculate(-1, 1) == 0
